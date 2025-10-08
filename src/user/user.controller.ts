7e99da1e748983c641761b04c84b7f508c80d692
waseem ahmad <waseemahmad9977@gmail.com>
2025-10-08 17:56:26 +0530

    debug

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,12 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { Request, Response } from 'express';
-import Exc from '../utill/exc.utill';
-import userModel from './user.model';
-import { UserInterface } from './user.interface';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import Exc from "../utill/exc.utill";
+import userModel from "./user.model";
+import { UserInterface } from "./user.interface";
 
 const FOURTEEN_MINUTE = 840000;
 const SIX_DAYS = 518400000;
-
 
 const getToken = (user: UserInterface) => {
   const payload = {
@@ -17,11 +16,17 @@ const getToken = (user: UserInterface) => {
     phone: user.phone,
     role: user.role,
   };
-  
-  console.log("check payload "+ payload)
 
-  const accessToken = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '15m' });
-  const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET as string, { expiresIn: '7d' });
+  console.log("check payload " + payload);
+
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign(
+    payload,
+    process.env.JWT_REFRESH_SECRET as string,
+    { expiresIn: "7d" }
+  );
 
   return { accessToken, refreshToken };
 };
@@ -29,9 +34,8 @@ const getToken = (user: UserInterface) => {
 export const signup = Exc(async (req: Request, res: Response) => {
   const newUser = new userModel(req.body);
   await newUser.save();
-  res.json({ message: 'Signup success' });
+  res.json({ message: "Signup success" });
 });
-
 
 export const login = Exc(async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -44,38 +48,36 @@ export const login = Exc(async (req: Request, res: Response) => {
 
   const { refreshToken, accessToken } = getToken(user);
 
-  console.log("checking"+refreshToken,accessToken)
-
- 
-  res.cookie('accessToken', accessToken, {
-    maxAge: FOURTEEN_MINUTE,
-    domain: process.env.NODE_ENV === 'dev' ? 'localhost' : process.env.DOMAIN,
-    secure: process.env.NODE_ENV !== 'dev',
-    httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'dev' ? 'lax' : 'none',
-  });
-
-  res.cookie('refreshToken', refreshToken, {
-    maxAge: SIX_DAYS,
-    domain: process.env.NODE_ENV === 'dev' ? 'localhost' : process.env.DOMAIN,
-    secure: process.env.NODE_ENV !== 'dev',
-    httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'dev' ? 'lax' : 'none',
-  });
   
-  
+  res.setHeader('Set-Cookie', [
+    `accessToken=${accessToken}; Path=/; HttpOnly; Secure; SameSite=None; Domain=${process.env.DOMAIN}`,
+    `refreshToken=${refreshToken}; Path=/; HttpOnly; Secure; SameSite=None; Domain=${process.env.DOMAIN}`
+  ]);
+
   res.json({ message: 'Login success', role: user.role });
 });
 
 
-
-
 export const logout = Exc((req: Request, res: Response) => {
-  res.cookie('accessToken', null, { maxAge: 0, domain: process.env.DOMAIN, secure: true, httpOnly: true, sameSite: process.env.NODE_ENV === 'dev' ? 'lax' : 'none'});
-  res.cookie('refreshToken', null, { maxAge: 0, domain: process.env.DOMAIN, secure: true, httpOnly: true, sameSite: process.env.NODE_ENV === 'dev' ? 'lax' : 'none'});
-  res.json({ message: 'Logout success' });
+  res.cookie("accessToken", null, {
+    maxAge: 0,
+    domain: process.env.DOMAIN,
+    secure: true,
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "dev" ? "lax" : "none",
+  });
+  res.cookie("refreshToken", null, {
+    maxAge: 0,
+    domain: process.env.DOMAIN,
+    secure: true,
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "dev" ? "lax" : "none",
+  });
+  res.json({ message: "Logout success" });
 });
 
-export const session = Exc((req: Request & { user?: UserInterface }, res: Response) => {
-  return res.json(req.user || null);
-});
+export const session = Exc(
+  (req: Request & { user?: UserInterface }, res: Response) => {
+    return res.json(req.user || null);
+  }
+);
